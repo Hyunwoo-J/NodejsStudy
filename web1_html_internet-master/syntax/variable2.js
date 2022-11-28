8c61f1a3b022278e60db19d070959c70d6179249
Hyunwoo <heoun3089@gmail.com>
2022-11-28 19:29:22 +0900

    Initial commit

---
+++ web1_html_internet-master/syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'egoing'
+var letter = 'Dear '+name+' egoing Lorem ipsum dolor sit amet, consectetur'+name+' adipisicing elit, sed do eiusmod'+name+' tempor incididunt egoing ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+console.log(letter);
